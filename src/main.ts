80c8fcb7e1ee47e6b4c63b72b6ecbe3ece8c6824
zhangjian <zhangjian@coding.net>
2022-01-26 11:18:32 +0800

    WIP
    Signed-off-by: zhangjian <zhangjian@coding.net>

--- src/main.ts
+++ src/main.ts
@@ -9,8 +9,6 @@ async function run(): Promise<void> {
     const password = core.getInput('password')
     const action = core.getInput('action')
 
-    core.debug(`host:${host},email:${email},password:${password},action:${action}`)
-
     const serve = NocalhostServe.single({ host, email, password })
 
     await serve.call(action)
